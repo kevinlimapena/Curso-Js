f9dc8de759dcee411794330b735b28836bd4be27
Kevin lima pena <kevinlimapena@gmail.com>
2024-12-30 11:39:32 -0400

    first commit

---
+++ Revisao30_12_24.js
@@ -0,0 +1,41 @@
+//Revisão das Aulas que ja assiti
+console.log("ola mundo")
+
+// Desafio
+
+{
+    console.log(
+        1,3,5,7,9
+
+    )
+}
+
+{
+    console.log(
+        2,4,6,8,10
+    )
+}
+
+// Var
+
+var quantidade = 10
+var preco = 6.4
+var imposto = 1.5
+var precofinal = quantidade + preco 
+console.log("Caneta")
+console.log(quantidade)
+console.log(preco)
+console.log(imposto)
+console.log(preco + imposto)
+console.log(precofinal)
+
+// Dados let
+
+let preco =19.90
+let desconto = 0.4 
+let precoComDisconto = preco * (1-desconto) 
+console.log(precoComDisconto)
+
+let nome = "Caderno"
+let categoria = "Papelaria"
+console.log("Produto: "+nome +", Categoria: "+ categoria)
